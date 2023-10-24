e5ee34dce1feaee177313e7b5c5c9d9a83d2a1a4
phan thanh loc <lopphanskt92@gmail.com>
2023-10-24 09:43:24 +0700

    add commit

--- src/app.js
+++ src/app.js
@@ -5,6 +5,10 @@ const path = require('path');
 const handlebars = require('express-handlebars');
 
 const port = 3000;
+app.use(express.static(path.join(__dirname, "public")))
+
+
+
 
 app.use(morgan('combined'));
 
@@ -17,6 +21,10 @@ app.set('views', path.join(__dirname, 'resources/views'));
 
 app.get('/', (req, res) => {
     res.render("home")
+
+});
+app.get('/tin-tuc', (req, res) => {
+    res.render("news")
 
 });
 
